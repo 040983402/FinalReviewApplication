7dbf949763935d67dab67ebf1771f0d5729e7342
simondaxia <abcde>
2021-08-16 00:07:53 -0400

    Initial Commit 5

--- app/src/main/java/com/example/finalreviewapplication/MainActivity.java
+++ app/src/main/java/com/example/finalreviewapplication/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.finalreviewapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       TextView mytext = findViewById(R.id.mytextview);
+       Button myButton = findViewById(R.id.mybutton);
+
+       myButton.setOnClickListener( ( vm ) -> mytext.setText("You click the button"));
+
+
+        CheckBox myCheck = findViewById(R.id.thecheckbox);
+        myCheck.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            Context context = getApplicationContext();
+            CharSequence text = "Nice Choice!";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context,text,duration);
+            toast.show();
+        });
+
+        CheckBox myCheck2 = findViewById(R.id.thecheckbox2);
+        myCheck2.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            Context context = getApplicationContext();
+            CharSequence text = "Also Nice Choice!";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context,text,duration);
+            toast.show();
+        });
+
+
+
+
+
 
     }
 }
